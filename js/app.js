6a3b1be0e43b50d37782d7ce419dd0bc79614fac
terakazu1997 <kazuki09910@gmail.com>
2019-07-25 22:08:29 +0900

    fix replaceText()

--- js/app.js
+++ js/app.js
@@ -72,13 +72,12 @@ var app  = new Vue({
             }
         },
         replaceText:function(str){
-            var chr ="";
-            if(str.match(/[\u30a1-\u30f6]/g)){
-                chr =String.fromCharCode(str.charCodeAt(0) - 0x60)
-            }
-            chr = str.match(/[ａ-ｚＡ-Ｚ０-９]/g)? String.fromCharCode(str.charCodeAt(0) - 65248): str
-            chr = chr.match(/[a-z]/g)? chr.toUpperCase(): chr;
-            return chr;
+            str =str.replace(/[ぁ-んａ-ｚＡ-Ｚ０-９]/g, function(chr) {
+                var replaceChr = chr.match(/[ぁ-ん]/g)?chr.charCodeAt(0) + 0x60:chr.charCodeAt(0) - 65248;
+                return String.fromCharCode(replaceChr);
+            })
+            str = str.match(/[a-z]/g)? str.toUpperCase(): str;
+            return str;
         }
     }
 })
